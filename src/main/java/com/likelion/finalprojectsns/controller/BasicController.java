cbace2ffac20d6d1e9e6f1c112466bfcd6548721
yejinstar <gyj5586@gmail.com>
2022-12-20 11:27:06 +0900

    feat: Swagger 3.0.0 추가

---
+++ src/main/java/com/likelion/finalprojectsns/controller/BasicController.java
@@ -0,0 +1,13 @@
+package com.likelion.finalprojectsns.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @GetMapping("api/v1/hello")
+    public String hello(){
+        return "hello";
+    }
+}
