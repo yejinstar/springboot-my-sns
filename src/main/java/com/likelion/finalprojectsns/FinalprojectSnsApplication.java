dacd1930c2f92c2d5236739e60cb9e4d67beebdd
yejinstar <gyj5586@gmail.com>
2022-12-20 10:54:50 +0900

    project setting

---
+++ src/main/java/com/likelion/finalprojectsns/FinalprojectSnsApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.finalprojectsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/finalprojectsns/FinalprojectSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.finalprojectsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
