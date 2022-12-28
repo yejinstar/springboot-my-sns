e3612b2d77caf95a7b7f7882ccc68dbae515ec58
yejinstar <gyj5586@gmail.com>
2022-12-28 10:41:38 +0900

    refactor: AppException.java - message 이용시 toString 대신 ErrorInfo 사용

--- src/main/java/com/likelion/finalprojectsns/exception/AppException.java
+++ src/main/java/com/likelion/finalprojectsns/exception/AppException.java
@@ -9,9 +9,4 @@ public class AppException extends RuntimeException {
     private ErrorCode errorCode;
     private String message;
 
-    @Override
-    public String toString() {
-        if(message == null) return errorCode.getMessage();
-        return String.format("%s %s", errorCode.getMessage(), message);
-    }
 }
