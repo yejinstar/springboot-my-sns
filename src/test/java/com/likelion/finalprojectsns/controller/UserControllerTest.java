37bf8d1bf7fdc5566078f23258f628e0843950d9
yejinstar <gyj5586@gmail.com>
2023-01-02 22:26:56 +0900

    test: UserControllerTest - login success - login fail - userName 없음 - login fail - password 틀림

--- src/test/java/com/likelion/finalprojectsns/controller/UserControllerTest.java
+++ src/test/java/com/likelion/finalprojectsns/controller/UserControllerTest.java
@@ -3,6 +3,8 @@ package com.likelion.finalprojectsns.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.likelion.finalprojectsns.domain.dto.UserJoinRequest;
 import com.likelion.finalprojectsns.domain.dto.UserJoinResponse;
+import com.likelion.finalprojectsns.domain.dto.UserLoginRequest;
+import com.likelion.finalprojectsns.domain.dto.UserLoginResponse;
 import com.likelion.finalprojectsns.exception.AppException;
 import com.likelion.finalprojectsns.exception.ErrorCode;
 import com.likelion.finalprojectsns.service.UserService;
@@ -81,20 +83,71 @@ class UserControllerTest {
     }
 
     @Test
+    @WithMockUser
     @DisplayName("login success")
     void login_success() throws Exception {
+        UserLoginRequest userLoginRequest = UserLoginRequest.builder()
+                .userName("userName")
+                .password("1234")
+                .build();
+        UserLoginResponse userLoginResponse = UserLoginResponse.builder()
+                .jwt("jwtToken")
+                .build();
+
+        when(userService.login(any())).thenReturn(userLoginResponse);
+
+        mockMvc.perform(post("/api/v1/users/login")
+                    .with(csrf())
+                    .content(objectMapper.writeValueAsBytes(userLoginRequest))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
 
     }
 
     @Test
+    @WithMockUser
     @DisplayName("login fail1 - userName 없음")
     void login_fail1() throws Exception {
+        UserLoginRequest userLoginRequest = UserLoginRequest.builder()
+                .userName("userName")
+                .password("1234")
+                .build();
+        UserLoginResponse userLoginResponse = UserLoginResponse.builder()
+                .jwt("jwtToken")
+                .build();
 
+        when(userService.login(any()))
+                .thenThrow(new AppException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()));
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .with(csrf())
+                        .content(objectMapper.writeValueAsBytes(userLoginRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(ErrorCode.USERNAME_NOT_FOUND.getStatus().value()))
+                .andDo(print());
     }
 
     @Test
+    @WithMockUser
     @DisplayName("login fail2 - password 틀림")
     void login_fail2() throws Exception {
+        UserLoginRequest userLoginRequest = UserLoginRequest.builder()
+                .userName("userName")
+                .password("1234")
+                .build();
+        UserLoginResponse userLoginResponse = UserLoginResponse.builder()
+                .jwt("jwtToken")
+                .build();
 
+        when(userService.login(any()))
+                .thenThrow(new AppException(ErrorCode.INVALID_PASSWORD, ErrorCode.INVALID_PASSWORD.getMessage()));
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .with(csrf())
+                        .content(objectMapper.writeValueAsBytes(userLoginRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(ErrorCode.INVALID_PASSWORD.getStatus().value()))
+                .andDo(print());
     }
 }
